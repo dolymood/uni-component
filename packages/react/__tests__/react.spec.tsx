3e8372a2731e89e815c79798734f3412f44634bc
dolymood <dolymood@gmail.com>
2021-12-09 15:20:56 +0800

    chore: test do not run build

--- packages/react/__tests__/react.spec.tsx
+++ packages/react/__tests__/react.spec.tsx
@@ -8,10 +8,17 @@ import {
   fireEvent,
   render
 } from '@testing-library/react'
+import { CubeButton } from '../example/button/button'
+import { CubeButtons } from '../example/buttons/buttons'
 
 afterEach(cleanup)
 
 describe('Test React', () => {
-  it('should work correctly', () => {
+  it('should work correctly - with button', () => {
+    
+  })
+  it('should work correctly - with buttons', () => {
+  })
+  it('should work correctly - with buttons & button', () => {
   })
 })
