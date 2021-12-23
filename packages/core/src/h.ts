e0b849555900ef5b5619eda430d1ca83087cc63e
dolymood <dolymood@gmail.com>
2021-12-23 16:43:45 +0800

    fix: h children

--- packages/core/src/h.ts
+++ packages/core/src/h.ts
@@ -2,10 +2,11 @@ import { getPlatform } from './platform'
 
 export function h (type: any, props: any, ...children: any) {
   const platform = getPlatform()
-  if (children.length === 1) {
-    children = children[0]
-  } else if (children.length === 0) {
+  if (children.length === 0) {
     children = props.children
+  }
+  if (children && !Array.isArray(children)) {
+    children = [children]
   }
   const vnode = platform.createVNode(type, props, children)
   return vnode
