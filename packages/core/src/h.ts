71104cb0500c8127d4aee6e9a560e9d6739dc5e9
dolymood <dolymood@gmail.com>
2021-12-23 16:33:00 +0800

    chore: platform & ref

--- packages/core/src/h.ts
+++ packages/core/src/h.ts
@@ -2,6 +2,11 @@ import { getPlatform } from './platform'
 
 export function h (type: any, props: any, ...children: any) {
   const platform = getPlatform()
+  if (children.length === 1) {
+    children = children[0]
+  } else if (children.length === 0) {
+    children = props.children
+  }
   const vnode = platform.createVNode(type, props, children)
   return vnode
 }
