d669f17737e1a39935a6e659fadd724a04e312df
dolymood <dolymood@gmail.com>
2021-12-27 11:44:24 +0800

    feat: movable area style

--- packages/components/example/pages/movable-area/index.tsx
+++ packages/components/example/pages/movable-area/index.tsx
@@ -2,10 +2,20 @@ import { h } from '@uni-component/core'
 import { MovableArea, MovableView } from '@uni-component/components'
 
 export default function MovableAreaDemo () {
+  const style = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '50px',
+    width: '50px',
+    background: '#1AAD19',
+    color: '#fff'
+  }
+  const Text = <div style={style}>text</div>
   return (
-    <div style={{position: 'relative', width: '100px', height: '100px', background: '#999'}}>
-      <MovableArea width={100} height={100}>
-        <MovableView>movable view</MovableView>
+    <div style={{position: 'relative', width: '200px', height: '200px', background: '#999'}}>
+      <MovableArea width={200} height={200}>
+        <MovableView x={10} y={10} direction='all'>{Text}</MovableView>
       </MovableArea>
     </div>
   )
