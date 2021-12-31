22e6595a9adc94deb9281b880b145d8a311af8b7
dolymood <dolymood@gmail.com>
2021-12-31 23:44:55 +0800

    refactor(vue): delete ueeless logic

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -6,32 +6,21 @@ import { uni2Vue } from './vue'
 
 export * from './vue'
 
-const transformPropsMap: Record<string, string> = {}
-const transformProps = Object.keys(transformPropsMap)
-
 setPlatform({
   createComponent: uni2Vue,
   createVNode: function (type, props, children) {
     const isPlainNode = typeof type === 'string'
-    isPlainNode && transformProps.forEach((key) => {
-      if (props.hasOwnProperty(key)) {
-        props[transformPropsMap[key]] = props[key]
+    isPlainNode && Object.keys(props).forEach((key) => {
+      if (key.indexOf('on') === 0) {
+        // process on events
+        const val = props[key]
         delete props[key]
+        const newKey = key.replace(/([A-Z][^A-Z]+)([A-Z].+)/, (_, name, ext) => {
+          return name + ext.toLowerCase()
+        })
+        props[newKey] = val
       }
     })
-    if (isPlainNode) {
-      Object.keys(props).forEach((key) => {
-        if (key.indexOf('on') === 0) {
-          // process on events
-          const val = props[key]
-          delete props[key]
-          const newKey = key.replace(/([A-Z][^A-Z]+)([A-Z].+)/, (_, name, ext) => {
-            return name + ext.toLowerCase()
-          })
-          props[newKey] = val
-        }
-      })
-    }
     // dev info
     delete props.__source
     delete props.__self
