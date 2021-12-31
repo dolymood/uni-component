3e05a0296f7cd353a77d30662e5e7893bb1babd3
dolymood <dolymood@gmail.com>
2021-12-31 23:28:12 +0800

    fix(vue): transform events

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -6,7 +6,37 @@ import { uni2Vue } from './vue'
 
 export * from './vue'
 
+const transformPropsMap: Record<string, string> = {}
+const transformProps = Object.keys(transformPropsMap)
+
 setPlatform({
   createComponent: uni2Vue,
-  createVNode
+  createVNode: function (type, props, children) {
+    const isPlainNode = typeof type === 'string'
+    isPlainNode && transformProps.forEach((key) => {
+      if (props.hasOwnProperty(key)) {
+        props[transformPropsMap[key]] = props[key]
+        delete props[key]
+      }
+    })
+    if (isPlainNode) {
+      Object.keys(props).forEach((key) => {
+        if (key.indexOf('on') === 0) {
+          // process on events
+          const val = props[key]
+          delete props[key]
+          const newKey = key.replace(/([A-Z][^A-Z]+)([A-Z].+)/, (_, name, ext) => {
+            return name + ext.toLowerCase()
+          })
+          props[newKey] = val
+        }
+      })
+    }
+    // dev info
+    delete props.__source
+    delete props.__self
+    return createVNode(type, props, {
+      default: () => children
+    })
+  }
 })
