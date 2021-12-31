dc7eb2354c1cf4eb5eb2445f1a52e8edaa6a6e30
dolymood <dolymood@gmail.com>
2021-12-31 10:58:19 +0800

    refactor(react): delete useless slot logic

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,7 +4,6 @@ import { uni2React } from './react'
 
 export * from './react'
 
-let isRenderingSlot = false
 const transformPropsMap: Record<string, string> = {
   class: 'className',
   autoplay: 'autoPlay',
@@ -23,27 +22,6 @@ setPlatform({
         delete props[key]
       }
     })
-    if (children && children.length) {
-      let i = children.length - 1
-      while (i >= 0) {
-        const child = children[i]
-        if (child && child.props && child.props.slot) {
-          if (!isRenderingSlot) {
-            if (!props.slots) {
-              props.slots = {}
-            }
-            props.slots[child.props.slot] = () => {
-              isRenderingSlot = true
-              return child
-            }
-            children.splice(i, 1)
-          } else {
-            isRenderingSlot = false
-          }
-        }
-        i--
-      }
-    }
     return createElement(type, props, ...children)
   }
 })
