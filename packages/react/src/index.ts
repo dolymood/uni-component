3671c6d7af3f5c3bc5f411400296ca1262fb0a69
dolymood <dolymood@gmail.com>
2022-02-09 11:20:41 +0800

    feat(react): svg attrs to camelized attr

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,5 +1,5 @@
 import { createElement, Fragment } from 'react'
-import { setPlatform, UniNode } from '@uni-component/core'
+import { camelize, setPlatform, UniNode } from '@uni-component/core'
 import { uni2React } from './react'
 
 export * from './react'
@@ -13,16 +13,86 @@ const transformPropsMap: Record<string, string> = {
   autofocus: 'autoFocus',
   readonly: 'readOnly',
   tabindex: 'tabIndex',
+  crossorigin: 'crossOrigin',
   novalidate: 'noValidate',
-  srcset: 'srcSet',
-  ariaLabel: 'aria-label',
-  ariaDisabled: 'aria-disabled',
-  ariaCurrent: 'aria-current',
-  'stroke-width': 'strokeWidth',
-  'stroke-dasharray': 'strokeDasharray',
-  'stroke-dashoffset': 'strokeDashoffset',
-  'inset-inline-start': 'insetInlineStart'
+  srcset: 'srcSet'
 }
+const svgAttrs = [
+  'accent-height',
+  'alignment-baseline',
+  'arabic-form',
+  'baseline-shift',
+  'cap-height',
+  'clip-path',
+  'clip-rule',
+  'color-interpolation',
+  'color-interpolation-filters',
+  'color-profile',
+  'color-rendering',
+  'dominant-baseline',
+  'enable-background',
+  'fill-opacity',
+  'fill-rule',
+  'flood-color',
+  'flood-opacity',
+  'font-family',
+  'font-size',
+  'font-size-adjust',
+  'font-stretch',
+  'font-style',
+  'font-variant',
+  'font-weight',
+  'glyph-name',
+  'glyph-orientation-horizontal',
+  'glyph-orientation-vertical',
+  'horiz-adv-x',
+  'horiz-origin-x',
+  'image-rendering',
+  'letter-spacing',
+  'lighting-color',
+  'marker-end',
+  'marker-mid',
+  'marker-start',
+  'overline-position',
+  'overline-thickness',
+  'paint-order',
+  'panose-1',
+  'pointer-events',
+  'shape-rendering',
+  'stop-color',
+  'stop-opacity',
+  'strikethrough-position',
+  'strikethrough-thickness',
+  'stroke-dasharray',
+  'stroke-dashoffset',
+  'stroke-linecap',
+  'stroke-linejoin',
+  'stroke-miterlimit',
+  'stroke-opacity',
+  'stroke-width',
+  'text-anchor',
+  'text-decoration',
+  'text-rendering',
+  'underline-position',
+  'underline-thickness',
+  'unicode-bidi',
+  'unicode-range',
+  'unitsPer-em',
+  'v-alphabetic',
+  'vector-effect',
+  'vert-adv-y',
+  'vert-origin-x',
+  'vert-origin-y',
+  'v-hanging',
+  'v-ideographic',
+  'v-mathematical',
+  'word-spacing',
+  'writing-mode',
+  'x-height'
+]
+svgAttrs.forEach((key) => {
+  transformPropsMap[key] = camelize(key)
+})
 const transformProps = Object.keys(transformPropsMap)
 
 setPlatform({
